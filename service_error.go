c0c9d6dcf24e3139c82ae0767bd0fc09cecd9731
风痕 <inkedawn@gmail.com>
2019-03-21 11:28:30 +0800

    add error ErrIllegalData

--- service_error.go
+++ service_error.go
@@ -5,6 +5,7 @@ var (
 	ErrWrongUsernameOrPassword = ServiceError{0, "用户名或者密码错误", ""}
 	ErrTokenExpired            = ServiceError{2, "超时，请重新登录", ""}
 	ErrDisqualifiedSpeed       = ServiceError{5, "速度不合格", ""}
+	ErrIllegalData             = ServiceError{100001, "非法数据", ""}
 )
 
 func init() {
@@ -12,6 +13,7 @@ func init() {
 	serviceErrorTable[1] = nil
 	serviceErrorTable[2] = ErrTokenExpired
 	serviceErrorTable[5] = ErrDisqualifiedSpeed
+	serviceErrorTable[100001] = ErrIllegalData
 }
 
 func translateServiceError(statusCode int64, statusMessage string) IServiceError {
