872b42f08ab264b3c2a5254552b630ecb45f83ea
风痕 <inkedawn@gmail.com>
2019-03-24 13:08:07 +0800

    SmartCreateRecordsAfter SmartCreateRecordsBefore

--- record.go
+++ record.go
@@ -87,68 +87,63 @@ func (r XTJsonSportTestData) GetStrpa() string {
 	return EncodeString(r.ToJSON())
 }
 
+// DEPRECATED, use SmartCreateRecordsBefore instead.
 func SmartCreateRecords(userID int64, schoolID int64, limitParams LimitParams, distance float64, beforeTime time.Time) []Record {
+	return SmartCreateRecordsBefore(schoolID, userID, limitParams, distance, beforeTime)
+}
+
+func SmartCreateRecordsAfter(schoolID int64, userID int64, limitParams LimitParams, distance float64, afterTime time.Time) []Record {
+	records := make([]Record, 0, int(distance/3))
+	remain := distance
+	lastEndTime := afterTime
+	println("distance", distance)
+	for remain > 0 {
+		singleDistance := smartCreateDistance(schoolID, userID, limitParams, remain)
+		if singleDistance == 0.0 {
+			break
+		}
+		// 时间间隔随机化
+		randomDuration := time.Duration(randRange(limitParams.MinuteDuration.Min, limitParams.MinuteDuration.Max)) * time.Minute
+		randomDuration += time.Duration(randRange(0, 60)) * time.Second // 时间间隔秒级随机化
+
+		beginTime := lastEndTime.Add(time.Duration(randRange(1, 30))*time.Minute + time.Duration(randRange(1, 60))*time.Second)
+		endTime := beginTime.Add(randomDuration)
+
+		records = append(records, Record{
+			UserID:    userID,
+			SchoolID:  schoolID,
+			Distance:  singleDistance,
+			BeginTime: beginTime,
+			EndTime:   endTime,
+			xtcode:    CalcXTcode(userID, toServiceStdTime(beginTime), toServiceStdDistance(singleDistance)),
+		})
+
+		remain -= singleDistance
+		lastEndTime = endTime
+	}
+	nRecord := len(records)
+	reverse := make([]Record, nRecord)
+	for i := 0; i < nRecord; i++ {
+		reverse[i] = records[nRecord-i-1]
+	}
+	return reverse
+}
+
+func SmartCreateRecordsBefore(schoolID int64, userID int64, limitParams LimitParams, distance float64, beforeTime time.Time) []Record {
 	records := make([]Record, 0, int(distance/3))
 	remain := distance
 	lastBeginTime := beforeTime
 	println("distance", distance)
 	for remain > 0 {
-		var singleDistance float64
-		// 范围取随机
-		// 会检查是否下一条可能丢弃较大的距离，防止：剩下比较多，但却不满足最小限制距离，不能生成下一条记录
-		if remain >= 2*limitParams.LimitSingleDistance.Max {
-			// 剩余足够大，正常取随机值
-			singleDistance = float64(randRange(int(limitParams.RandDistance.Min*1000), int(limitParams.RandDistance.Max*1000))) / 1000
-			println("p1", singleDistance)
-		} else if remain >= 2*limitParams.LimitSingleDistance.Min {
-			// 即将耗尽，首先尝试放入一条记录内，否则为下一条预留
-			if remain <= limitParams.LimitSingleDistance.Max {
-				singleDistance = remain
-			} else {
-				// 为下一条预留最小限制距离
-				singleDistance = remain - limitParams.LimitSingleDistance.Min
-			}
-			println("p2", singleDistance)
-		} else if remain >= limitParams.LimitSingleDistance.Min {
-			// 剩余的符合最小限制距离，直接使用剩余的生成最后一条记录
-			singleDistance = remain
-			println("p3", singleDistance)
-		} else if remain > 0.1 {
-			println("检查算法正确性", remain)
-			break
-		} else {
-			// 最后的零星距离，可以直接丢弃
+		singleDistance := smartCreateDistance(schoolID, userID, limitParams, remain)
+		if singleDistance == 0.0 {
 			break
 		}
-		// 小数部分随机化 -0.09 ~ 0.09
-		tinyPart := float64(randRange(0, 99999)) / 1000000
-		switch r := singleDistance + tinyPart; {
-		case r < limitParams.LimitSingleDistance.Min:
-			singleDistance = limitParams.LimitSingleDistance.Min
-			/*case r > userInfo.LimitParams.LimitSingleDistance.Max:
-			singleDistance = userInfo.LimitParams.LimitSingleDistance.Max
-			*/
-		default:
-			singleDistance += tinyPart
-		}
-
-		// 检测结果合法性，由于TinyPart允许上下浮动0.1
-		if singleDistance < limitParams.LimitSingleDistance.Min-0.1 {
-			// 丢弃不合法距离
-			log.Println("Drop distance: ", singleDistance)
-			continue
-		}
-		if singleDistance > limitParams.LimitSingleDistance.Max {
-			singleDistance = limitParams.LimitSingleDistance.Max
-		}
-
-		var randomDuration time.Duration
 		// 时间间隔随机化
-		randomDuration = time.Duration(randRange(limitParams.MinuteDuration.Min, limitParams.MinuteDuration.Max)) * time.Minute
+		randomDuration := time.Duration(randRange(limitParams.MinuteDuration.Min, limitParams.MinuteDuration.Max)) * time.Minute
 		randomDuration += time.Duration(randRange(0, 60)) * time.Second // 时间间隔秒级随机化
 
-		endTime := lastBeginTime.Add(-time.Duration(randRange(1, 30)) * time.Minute)
-		endTime = endTime.Add(-time.Duration(randRange(1, 60)) * time.Second)
+		endTime := lastBeginTime.Add(-time.Duration(randRange(1, 30))*time.Minute - time.Duration(randRange(1, 60))*time.Second)
 		beginTime := endTime.Add(-randomDuration)
 
 		records = append(records, Record{
@@ -160,7 +155,7 @@ func SmartCreateRecords(userID int64, schoolID int64, limitParams LimitParams, d
 			xtcode:    CalcXTcode(userID, toServiceStdTime(beginTime), toServiceStdDistance(singleDistance)),
 		})
 
-		remain -= singleDistance - tinyPart
+		remain -= singleDistance
 		lastBeginTime = beginTime
 	}
 	nRecord := len(records)
@@ -170,6 +165,59 @@ func SmartCreateRecords(userID int64, schoolID int64, limitParams LimitParams, d
 	}
 	return reverse
 }
+
+func smartCreateDistance(schoolID int64, userID int64, limitParams LimitParams, remain float64) (singleDistance float64) {
+	// 范围取随机
+	// 会检查是否下一条可能丢弃较大的距离，防止：剩下比较多，但却不满足最小限制距离，不能生成下一条记录
+	if remain >= 2*limitParams.LimitSingleDistance.Max {
+		// 剩余足够大，正常取随机值
+		singleDistance = float64(randRange(int(limitParams.RandDistance.Min*1000), int(limitParams.RandDistance.Max*1000))) / 1000
+		println("p1", singleDistance)
+	} else if remain >= 2*limitParams.LimitSingleDistance.Min {
+		// 即将耗尽，首先尝试放入一条记录内，否则为下一条预留
+		if remain <= limitParams.LimitSingleDistance.Max {
+			singleDistance = remain
+		} else {
+			// 为下一条预留最小限制距离
+			singleDistance = remain - limitParams.LimitSingleDistance.Min
+		}
+		println("p2", singleDistance)
+	} else if remain >= limitParams.LimitSingleDistance.Min {
+		// 剩余的符合最小限制距离，直接使用剩余的生成最后一条记录
+		singleDistance = remain
+		println("p3", singleDistance)
+	} else if remain > 0.1 {
+		println("检查算法正确性", remain)
+		return 0.0
+	} else {
+		// 最后的零星距离，可以直接丢弃
+		return 0.0
+	}
+
+	// 小数部分随机化 -0.09 ~ 0.09
+	tinyPart := float64(randRange(0, 99999)) / 1000000
+	switch r := singleDistance + tinyPart; {
+	case r < limitParams.LimitSingleDistance.Min:
+		singleDistance = limitParams.LimitSingleDistance.Min
+		/*case r > userInfo.LimitParams.LimitSingleDistance.Max:
+		singleDistance = userInfo.LimitParams.LimitSingleDistance.Max
+		*/
+	default:
+		singleDistance += tinyPart
+	}
+
+	// 检测结果合法性，由于TinyPart允许上下浮动0.1
+	if singleDistance < limitParams.LimitSingleDistance.Min-0.1 {
+		// 丢弃不合法距离
+		log.Println("Drop distance: ", singleDistance)
+		return 0.0
+	}
+	if singleDistance > limitParams.LimitSingleDistance.Max {
+		singleDistance = limitParams.LimitSingleDistance.Max
+	}
+	return singleDistance
+}
+
 func CreateRecord(userID int64, schoolID int64, distance float64, endTime time.Time, duration time.Duration) Record {
 	r := Record{
 		UserID:    userID,
--- record_test.go
+++ record_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func TestSmartCreateRecords(t *testing.T) {
+func TestSmartCreateRecordsBefore(t *testing.T) {
 	tests := []struct {
 		sex string
 	}{
@@ -15,7 +15,26 @@ func TestSmartCreateRecords(t *testing.T) {
 		t.Run(test.sex, func(t *testing.T) {
 			t.Logf(test.sex)
 			limit := GetDefaultLimitParams(test.sex)
-			records := SmartCreateRecords(0, 0, limit, limit.LimitTotalDistance.Max-0.1, time.Now())
+			records := SmartCreateRecordsBefore(0, 0, limit, limit.LimitTotalDistance.Max-0.1, time.Now())
+			for _, r := range records {
+				t.Logf("%+v", r)
+			}
+
+		})
+	}
+}
+
+func TestSmartCreateRecordsAfter(t *testing.T) {
+	tests := []struct {
+		sex string
+	}{
+		{"F"}, {"M"},
+	}
+	for _, test := range tests {
+		t.Run(test.sex, func(t *testing.T) {
+			t.Logf(test.sex)
+			limit := GetDefaultLimitParams(test.sex)
+			records := SmartCreateRecordsAfter(0, 0, limit, limit.LimitTotalDistance.Max-0.1, time.Now())
 			for _, r := range records {
 				t.Logf("%+v", r)
 			}
