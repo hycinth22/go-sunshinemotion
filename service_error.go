78813eed585022950e49ce77675a13f367df32de
风痕 <inkedawn@gmail.com>
2019-03-24 15:23:51 +0800

    add error ErrForbidden

--- service_error.go
+++ service_error.go
@@ -2,6 +2,7 @@ package ssmt
 
 var (
 	serviceErrorTable          = make(map[int64]IServiceError)
+	ErrForbidden               = ServiceError{-1, "无权进行此操作，可能不在跑步时间或被封禁", ""}
 	ErrWrongUsernameOrPassword = ServiceError{0, "用户名或者密码错误", ""}
 	ErrTokenExpired            = ServiceError{2, "超时，请重新登录", ""}
 	ErrDisqualifiedSpeed       = ServiceError{5, "速度不合格", ""}
@@ -9,6 +10,7 @@ var (
 )
 
 func init() {
+	serviceErrorTable[-1] = ErrForbidden
 	serviceErrorTable[0] = ErrWrongUsernameOrPassword
 	serviceErrorTable[1] = nil
 	serviceErrorTable[2] = ErrTokenExpired
