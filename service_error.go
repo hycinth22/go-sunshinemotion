1ee6b229a487be90a7ec962ab9ddc01de21e3bab
风痕 <inkedawn@gmail.com>
2019-03-25 10:51:27 +0800

    ErrInvalidToken

--- service_error.go
+++ service_error.go
@@ -4,7 +4,8 @@ var (
 	serviceErrorTable          = make(map[int64]IServiceError)
 	ErrForbidden               = ServiceError{-1, "无权进行此操作，可能不在跑步时间或被封禁", ""}
 	ErrWrongUsernameOrPassword = ServiceError{0, "用户名或者密码错误", ""}
-	ErrTokenExpired            = ServiceError{2, "超时，请重新登录", ""}
+	ErrInvalidToken            = ServiceError{2, "无效的登录信息", ""}
+	ErrTokenExpired            = ServiceError{2, "无效的登录信息", ""} // DEPRECATED
 	ErrDisqualifiedSpeed       = ServiceError{5, "速度不合格", ""}
 	ErrIllegalData             = ServiceError{100001, "非法数据", ""}
 )
@@ -13,7 +14,7 @@ func init() {
 	serviceErrorTable[-1] = ErrForbidden
 	serviceErrorTable[0] = ErrWrongUsernameOrPassword
 	serviceErrorTable[1] = nil
-	serviceErrorTable[2] = ErrTokenExpired
+	serviceErrorTable[2] = ErrInvalidToken
 	serviceErrorTable[5] = ErrDisqualifiedSpeed
 	serviceErrorTable[100001] = ErrIllegalData
 }
