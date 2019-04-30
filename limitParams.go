689fcdbd26b747a9833a7bcf7dade9d184cf454e
风痕 <inkedawn@gmail.com>
2019-04-30 13:57:36 +0800

    新增LimitTotalMaxDistance，弃用LimitTotalDistance区间

--- limitParams.go
+++ limitParams.go
@@ -5,8 +5,11 @@ type LimitParams struct {
 	RandDistance Float64Range
 	// 限制区间（生成单条记录的的硬性限制）
 	LimitSingleDistance Float64Range
+	// DEPRECATED: Use LimitTotalMaxDistance instead
 	// 限制区间（所有记录总和的限制区间）
 	LimitTotalDistance Float64Range
+	// 所有记录总和的最大值
+	LimitTotalMaxDistance float64
 	// 每公里的分钟系数区间
 	MinutePerKM Float64Range
 }
@@ -22,17 +25,19 @@ func GetDefaultLimitParams(sex string) LimitParams {
 	switch sex {
 	case "F":
 		return LimitParams{
-			RandDistance:        Float64Range{2.5, 3.0},
-			LimitSingleDistance: Float64Range{1.0, 3.0},
-			LimitTotalDistance:  Float64Range{1.0, 3.0},
-			MinutePerKM:         Float64Range{7.0, 13.0},
+			RandDistance:          Float64Range{2.5, 3.0},
+			LimitSingleDistance:   Float64Range{1.0, 3.0},
+			LimitTotalDistance:    Float64Range{1.0, 3.0},
+			LimitTotalMaxDistance: 3.0,
+			MinutePerKM:           Float64Range{7.0, 13.0},
 		}
 	case "M":
 		return LimitParams{
-			RandDistance:        Float64Range{4.5, 5.0},
-			LimitSingleDistance: Float64Range{2.0, 5.0},
-			LimitTotalDistance:  Float64Range{2.0, 5.0},
-			MinutePerKM:         Float64Range{8.0, 14.0},
+			RandDistance:          Float64Range{4.5, 5.0},
+			LimitSingleDistance:   Float64Range{2.0, 5.0},
+			LimitTotalDistance:    Float64Range{2.0, 5.0},
+			LimitTotalMaxDistance: 5.0,
+			MinutePerKM:           Float64Range{8.0, 14.0},
 		}
 	}
 	return LimitParams{}
