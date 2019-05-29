19df884d26936569d441f82f491fdc916841ed99
风痕 <inkedawn@gmail.com>
2019-05-29 19:10:38 +0800

    更新服务错误

--- service_error.go
+++ service_error.go
@@ -1,13 +1,15 @@
 package ssmt
 
 var (
-	serviceErrorTable          = make(map[int64]IServiceError)
-	ErrForbidden               = ServiceError{-1, "无权进行此操作，可能不在跑步时间或被封禁", ""}
-	ErrWrongUsernameOrPassword = ServiceError{0, "用户名或者密码错误，或体能测试未开放", ""}
-	ErrInvalidToken            = ServiceError{2, "无效的登录信息", ""}
-	ErrTokenExpired            = ServiceError{2, "无效的登录信息", ""} // DEPRECATED
-	ErrDisqualifiedSpeed       = ServiceError{5, "速度不合格", ""}
-	ErrIllegalData             = ServiceError{100001, "非法数据", ""}
+	serviceErrorTable    = make(map[int64]IServiceError)
+	ErrForbidden         = ServiceError{-1, "禁止进行此操作", ""} // 无权进行此操作，可能不在跑步时间或帐号被封禁
+	ErrInvalidIdentity   = ServiceError{0, "身份信息错误", ""}
+	ErrInvalidToken      = ServiceError{2, "令牌信息无效", ""}
+	ErrDisqualifiedSpeed = ServiceError{5, "速度不合格", ""}
+	ErrIllegalData       = ServiceError{100001, "非法数据", ""}
+
+	ErrTokenExpired            = ServiceError{2, "无效的登录信息", ""}            // DEPRECATED
+	ErrWrongUsernameOrPassword = ServiceError{0, "用户名或者密码错误，或体能测试未开放", ""} // DEPRECATED 用户名或者密码错误，或体能测试未开放
 )
 
 func init() {
